3f179d27b25fbf7f9f300aa6d494e6e228a89534
Alexis <alexis.oculi@gmail.com>
2025-10-29 22:04:11 +0700

    update

--- apps/extension/entrypoints/hooks/auth.ts
+++ apps/extension/entrypoints/hooks/auth.ts
@@ -1,9 +1,8 @@
 /**
  * Main auth hook - coordinates all auth operations
  */
-import { useLogin } from './useLogin';
 import { useLoginAndUnlock } from './useLoginAndUnlock';
 import { useLogout } from './useLogout';
 import { useSession } from './useSession';
 
-export { useLogin, useLoginAndUnlock, useLogout, useSession };
+export { useLoginAndUnlock, useLogout, useSession };
--- apps/extension/entrypoints/hooks/bookmarks.ts
+++ apps/extension/entrypoints/hooks/bookmarks.ts
@@ -4,6 +4,6 @@
 import { MAX_NOTES_LENGTH, MAX_TAGS_PER_ITEM } from '../lib/validation';
 import { useBookmarks } from './useBookmarks';
 import { useTags } from './useTags';
-import { useBookmarkValidation, useManifestSize, useTagValidation } from './validation';
+import { useManifestSize } from './validation';
 
-export { MAX_NOTES_LENGTH, MAX_TAGS_PER_ITEM, useBookmarks, useBookmarkValidation, useManifestSize, useTags, useTagValidation };
+export { MAX_NOTES_LENGTH, MAX_TAGS_PER_ITEM, useBookmarks, useManifestSize, useTags };
